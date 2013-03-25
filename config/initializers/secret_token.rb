4209e7a9b0e1ae71a343b72bbf20dc6f1a5124d6
arif <arif.poonawala@gmail.com>
2013-03-25 10:54:44 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breakingbad::Application.config.secret_token = 'd30825067ac75f6d79b29cb9652645a94ed40f7dbb9f912fadf5d848fd572e829059fcc8cb20736af6af59275c25ef8d57763d1faf2889737f4c7a9550ccf30b'
